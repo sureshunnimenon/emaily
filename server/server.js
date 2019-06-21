4a7038e0de8a249460c4ca7e7f7b28a8e55149fd
sureshunnimenon <suresh.unni.menon@gmail.com>
2019-06-21 09:59:46 +0300

    changed greeting

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express= require('express')
 const app = express();
 
-app.get('/', (req,res) => res.send({Hi: "there"}))
+app.get('/', (req,res) => res.send({Hi: "there I deployed and made a change!"}))
 
 const PORT = process.env.PORT || 5000
 
